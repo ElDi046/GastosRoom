8f7f1f99de5c34e93f67843e4935d5a732800eca
ElDi046 <47604203+ElDi046@users.noreply.github.com>
2022-04-26 16:13:37 -0500

    Si

--- app/src/main/java/edu/itesm/gastos/adapter/GastoViewHolder.kt
+++ app/src/main/java/edu/itesm/gastos/adapter/GastoViewHolder.kt
@@ -9,6 +9,6 @@ class GastoViewHolder (view: View): RecyclerView.ViewHolder(view) {
     private val binding = GastoBinding.bind(view)
     fun bind(gasto:Gasto){
         binding.concepto.text = gasto.description
-        binding.monto.text = "${ gasto.monto }"
+        binding.monto.text = "${ gasto.amount }"
     }
 }
--- app/src/main/java/edu/itesm/gastos/dao/GastoDao.kt
+++ app/src/main/java/edu/itesm/gastos/dao/GastoDao.kt
@@ -3,3 +3,13 @@ package edu.itesm.gastos.dao
 import androidx.room.*
 import edu.itesm.gastos.entities.Gasto
 
+@Dao
+interface GastoDao{
+      @Query("SELECT * FROM Gasto")
+      suspend fun getAllGastos():List<Gasto>
+
+      //"select sum(amount) from Gasto" <--- Saca la sumatoria de los valores de la columna
+
+      @Insert
+      suspend fun insertGasto(gasto: Gasto)
+}
